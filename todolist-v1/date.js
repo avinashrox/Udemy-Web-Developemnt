7082d2772e9914c8f1fb4441b9a6c3fc489a8bc6
avinashrox <avinashsenthil029@gmail.com>
2022-01-19 04:04:28 -0700

    Coursework

---
+++ todolist-v1/date.js
@@ -0,0 +1,19 @@
+
+module.exports.getDate = function (){
+  let today = new Date();
+  let options={
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  };
+  let day=today.toLocaleDateString("en-US",options)
+  return day;
+}
+module.exports.getDay = function getDay(){
+  let today = new Date();
+  let options={
+    weekday:"long"
+  };
+  let day=today.toLocaleDateString("en-US",options)
+  return day;
+}
